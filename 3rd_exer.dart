930298b2708e4ad2c0991a3dacb7adb2c82be6fc
BadrEssalam <badressalambdrilling@gmail.com>
2025-01-13 21:59:37 +0100

    remove the solution suggested by the exercice writer

--- 3rd_exer.dart
+++ 3rd_exer.dart
@@ -7,46 +7,28 @@ TODO:
 */
 
 //* My solution
-// void main() {
-
-//   List<int> GivenList = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-//   List<int> EvenElements = [];
-
-//   EvenElements = EvenElementsExtract(GivenList);
-//   print("Even Elements $EvenElements");
-// }
-
-// List<int> EvenElementsExtract(List<int> Elements){
-  
-//   List<int> EvenElementsFun = [];
-
-//   for (int element in Elements) {
-//     if (element.isEven) {
-//       EvenElementsFun.add(element);
-//       //! ev_el[e] = a[i]; this way is an error
-//     }
-//   }
-
-//   //* Other Methods
-//   //* EvenElementsFun = Elements.where((value) => value.isEven).toList();
-//   //* EvenElementsFun = Elements.takeWhile((value) => value.isEven).toList();
-//   return EvenElementsFun;
-// }
-
-// the suggested sol from the author
-// /*
 void main() {
-  List<int> a = [1, 4, 9, 16, 25, 4, 36, 49, 64, 81, 100];
-  List<int> l = [];
 
-  for (var e in a) {
-    if (e % 2 == 0) {
-      l.add(e);
+  List<int> GivenList = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  List<int> EvenElements = [];
+
+  EvenElements = EvenElementsExtract(GivenList);
+  print("Even Elements $EvenElements");
+}
+
+List<int> EvenElementsExtract(List<int> Elements){
+  
+  List<int> EvenElementsFun = [];
+
+  for (int element in Elements) {
+    if (element.isEven) {
+      EvenElementsFun.add(element);
+      //! ev_el[e] = a[i]; this way is an error
     }
   }
-  print(l);
-  
-  // One liner
-  print([for (var e in a) if (++e % 2 == 0) e]);
+
+  //* Other Methods
+  //* EvenElementsFun = Elements.where((value) => value.isEven).toList();
+  //* EvenElementsFun = Elements.takeWhile((value) => value.isEven).toList();
+  return EvenElementsFun;
 }
-// */
