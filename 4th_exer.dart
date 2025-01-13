671934becef643b9e37d76046c7ceb6c2f1f9c4a
BadrEssalam <badressalambdrilling@gmail.com>
2025-01-13 22:07:04 +0100

    add another built for the FirstLast() function

--- 4th_exer.dart
+++ 4th_exer.dart
@@ -1,9 +1,9 @@
-
-// Write a program that takes a list of numbers for example
-// a = [5, 10, 15, 20, 25]
-// and makes a new list of only the first and last elements
-// of the given list. For practice, write this code inside a function.
-
+/*
+* Write a program that takes a list of numbers for example
+* a = [5, 10, 15, 20, 25]
+* and makes a new list of only the first and last elements
+* of the given list. For practice, write this code inside a function.
+*/
 void main() {
   List Elements = [5, 10, 15, 20, 25];
 
@@ -11,6 +11,7 @@ void main() {
   print("First Element ${FirstLastList[0]}, Last Element ${FirstLastList[1]}");
 }
 
-List FirstLast(Element) {
-  return [Element.first, Element.last];
+List FirstLast(Elements) {
+  // return [Elements.first, Elements.last];
+   return [Elements[0], Elements[Elements.length-1]];
 }
