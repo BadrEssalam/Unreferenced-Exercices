b5e49475235f16de18d8e137edc60e8388049cff
BadrEssalam <badressalambdrilling@gmail.com>
2025-01-13 22:56:48 +0100

    add comments specify the list elements types

--- 4th_exer.dart
+++ 4th_exer.dart
@@ -1,17 +1,20 @@
 /*
+ToDo:
 * Write a program that takes a list of numbers for example
 * a = [5, 10, 15, 20, 25]
 * and makes a new list of only the first and last elements
 * of the given list. For practice, write this code inside a function.
 */
-void main() {
-  List Elements = [5, 10, 15, 20, 25];
 
-  List FirstLastList = FirstLast(Elements);
+//* My solution
+void main() {
+  List<int> Elements = [5, 10, 15, 20, 25];
+  List<int> FirstLastList = FirstLast(Elements);
+
   print("First Element ${FirstLastList[0]}, Last Element ${FirstLastList[1]}");
 }
 
-List FirstLast(Elements) {
-  // return [Elements.first, Elements.last];
-   return [Elements[0], Elements[Elements.length-1]];
+List<int> FirstLast(List<int> Elements) {
+  return [Elements.first, Elements.last];
+  //* return [Elements[0], Elements[Elements.length-1]];
 }
